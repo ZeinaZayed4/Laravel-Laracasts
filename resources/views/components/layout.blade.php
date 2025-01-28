82339f81000b5f6e55816c69b2886ba79c0cf0aa
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-01-28 03:33:34 +0200

    Create the obligatory three page layout
    using layout files

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+</head>
+<body>
+    <x-nav-link>
+        <x-nav-link>Home</x-nav-link>
+        <x-nav-link>About</x-nav-link>
+        <x-nav-link>Contact</x-nav-link>
+    </x-nav-link>
+    {{ $slot }}
+</body>
+</html>
---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,3 @@
+<nav>
+    <a href="/">{{ $slot }}</a>
+</nav>
